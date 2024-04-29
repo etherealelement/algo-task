eb299f88c1ea7d6788280cb5570cf3d1b423d7d3
etherealelement <vitalik.safonov@list.ru>
2024-04-29 12:12:01 +0300

    task1

--- task1.js
+++ task1.js
@@ -1,26 +1,19 @@
-let worker = {
-	someMethod() {
-		return 1;
-	},
-
-	slow(x) {
-		console.log('Called with ' + x);
-		return x * this.someMethod(); // (*)
-	},
+let slow = x => {
+	console.log(x);
+	return x;
 };
 
 const cachingDecorator = func => {
-	let cache = new Map();
+	const cache = new Map();
 
-	return function (x) {
+	return function (...x) {
 		if (cache.has(x)) {
 			return cache.get(x);
 		}
-		let result = func.call(this, x);
+		const result = func(...x);
 		cache.set(x, result);
-		return result;
 	};
 };
 
-worker.slow = cachingDecorator(worker.slow);
-console.log(worker.slow(1));
+slow = cachingDecorator(slow);
+console.log(slow(1));
