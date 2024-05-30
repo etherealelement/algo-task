1fbbb5e15cc9ee19d2146173af63204f1a96d9fa
etherealelement <vitalik.safonov@list.ru>
2024-05-30 08:36:10 +0300

    fist sol task30

---
+++ task30.js
@@ -0,0 +1,16 @@
+const tree = {
+	value: 8,
+	left: {
+		value: 6,
+		left: null,
+		right: null,
+	},
+};
+
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
