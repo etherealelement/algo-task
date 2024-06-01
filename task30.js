5bf1ae05ba90eff56f64872485badb381982890e
etherealelement <vitalik.safonov@list.ru>
2024-06-01 07:22:18 +0300

    first sol tasks 30 32

--- task30.js
+++ task30.js
@@ -53,7 +53,38 @@ class BinaryTree {
 			}
 		}
 	}
+
+	preOrder(node, callback) {
+		if (!node) return;
+
+		if (callback) {
+			callback(node);
+		}
+
+		this.preOrder(node.left, callback);
+		this.preOrder(node.right, callback);
+	}
+
+	inOrder(node, callback) {}
+	postOrder(node, callback) {}
+	traverseDFS(callback, method) {
+		if (method === 'preOrder') {
+			return this.preOrder(this.root, callback);
+		}
+
+		if (method === 'inOrder') {
+			return this.inOrder(this.root, callback);
+		}
+
+		return this.postOrder(this.root, callback);
+	}
+	traverseBFS() {}
 }
 
 const myTree = new BinaryTree();
-myTree.add(8), myTree.add(7), console.log(myTree);
+[1, 2, 3, 4, 5, 6, 7, 8, 10, 20].forEach(item => myTree.add(item));
+myTree.traverseDFS(() => console.log(node.value), 'inOrder');
+myTree.traverseDFS(() => console.log(node.value), 'postOrder');
+myTree.traverseDFS(() => console.log(node.value), 'preOrder');
+
+console.log(myTree);
---
+++ task32.js
@@ -0,0 +1,8 @@
+var maxDepth = function (root) {
+	if (!root) return 0;
+	let left = maxDepth(root.left);
+	let right = maxDepth(root.right);
+
+	return Math.max(left, right) + 1;
+};
+console.log(maxDepth([3, 9, 20, null, null, 15, 7]));
